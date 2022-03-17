b900bfbadbd934bd0d27129eea95caa16f9cdc8a
selim <68208055+seohl16@users.noreply.github.com>
2022-03-17 14:52:15 +0900

    W2_Thu_selim_solution

--- week2/Thu/SeohyunLim_heap.py
+++ week2/Thu/SeohyunLim_heap.py
@@ -5,11 +5,10 @@ import heapq
 import math 
 
 def solution(jobs):
-    org = jobs.copy()
     jobs = sorted(jobs)
+    org = jobs.copy()
     end = jobs[0][0]
     heap = []
-    idx = 0
     answer = 0
     first = jobs.pop(0)
     heapq.heappush(heap, [first[1], first[0]])
@@ -18,14 +17,11 @@ def solution(jobs):
         item = heapq.heappop(heap)
         answer += end + item[0] - item[1]
         end += item[0] # 걸리는 시간 더함 
-        print(end, answer)
-        while (jobs and jobs[0][0] <= end):
             out = jobs.pop(0)
             heapq.heappush(heap, [out[1], out[0]])
         if len(heap) == 0 and jobs:
             out = jobs.pop(0)
             end = out[0]
             heapq.heappush(heap, [out[1], out[0]])
-    print(end, answer)
     answer = math.floor(answer / len(org))
     return answer
