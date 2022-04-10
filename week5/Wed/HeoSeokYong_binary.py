e11a2adcde13a36e09e19d2d52b4da04f61e9628
HeoSeokYong <hsdragoner@gmail.com>
2022-04-10 19:26:05 +0900

    W03_Wed_HeoSeokYong_binary

---
+++ week5/Wed/HeoSeokYong_binary.py
@@ -0,0 +1,20 @@
+def solution(n, times):
+    times.sort()
+    answer = 0
+
+    left = 1
+    right = n * times[-1]
+    
+    while left <= right:
+        mid = (left+right) // 2
+        check = 0
+        for t in times:
+            check += mid // t
+            if check >= n:
+                answer = mid
+                right = mid - 1
+                break
+        if check < n:
+            left = mid + 1
+    
+    return answer
