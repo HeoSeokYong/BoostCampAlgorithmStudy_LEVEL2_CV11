fceca5bb74062e50e69dd6a026cc1a5ea166a27e
selim <68208055+seohl16@users.noreply.github.com>
2022-05-02 10:08:08 +0900

    W9_Mon_selim_solution

---
+++ week9/Mon/selim_bfs.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+n, k = map(int, input().split())
+# print(n, k)
+def solution(n, k):
+    if n == k:
+        return 0
+    else : 
+        visited = [0] * 100001
+        que = deque()
+        que.append([n, 0])
+        while que:
+            now, lev = que.popleft()
+            for newx in [now+1, now-1, now*2]:
+                if newx == k:
+                    return lev+1
+                if newx >= 0 and newx <= 100000 and visited[newx] == 0:
+                    visited[newx] = 1
+                    que.append([newx, lev+1])
+print(solution(n, k))
