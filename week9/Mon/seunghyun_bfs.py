a104756d477e1488db4dd4d72e2821b9400db7a9
sseunghyuns <allindata96@gmail.com>
2022-05-02 11:06:20 +0900

    W09_Mon_seunghyun_bfs

---
+++ week9/Mon/seunghyun_bfs.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+q = deque()
+level = 0
+visited = [False] * 100001
+q.append((n, level))
+
+while q:
+    now, lvl = q.popleft()
+    visited[now] = True
+    
+    if now==k:
+        break
+    else:
+        if 0<=now-1 <= 100000 and not visited[now-1]:
+            q.append((now-1, lvl+1))
+        if 0<=now+1 <= 100000 and not visited[now+1]:
+            q.append((now+1, lvl+1))
+        if 0<=now*2 <= 100000 and not visited[now*2]:
+            q.append((now*2, lvl+1))
+
+print(lvl)     
